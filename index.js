5887e375175c54aea3affbb0afd66bc4af202459
Crawlerop <59941517+Crawlerop@users.noreply.github.com>
2022-11-15 18:07:57 +0700

    Add EH

--- index.js
+++ index.js
@@ -52,151 +52,163 @@ app.get("/dtv/id", (req, res) => {
 })
 
 app.get("/dtv/id/region", async (req, res) => {
-    var [target_lat, target_lng] = [0,0]
-    if (req.query.ip) {
-        const lookup_result = geoip.lookup(req.query.ip)                
-        if (!lookup_result || lookup_result.country.toUpperCase() != "ID") {
-            return res.status(200).json({"region":"N/A"})
-        }
-        [target_lat, target_lng] = lookup_result.ll
-    } else if (req.query.lat && req.query.lng) {        
-        const lookup_result = await nominatim.reverse({lat: parseFloat(req.query.lat), lon: parseFloat(req.query.lng), zoom: 16})                
-        if (lookup_result.error || lookup_result.address.country_code.toUpperCase() != "ID") {
-            return res.status(200).json({"region":"N/A"})
-        }
-        [target_lat, target_lng] = [parseFloat(req.query.lat), parseFloat(req.query.lng)]
-    } else {
-        const lookup_result = geoip.lookup(req.ip)         
-        if (!lookup_result || lookup_result.country != "ID") {
-            return res.status(200).json({"region":"N/A"})            
-        }
-        [target_lat, target_lng] = lookup_result.ll
-    }        
-    const transmitters = await axios.get("https://digitaltv.kominfo.go.id/executive/get_antenna_map")
-    const tx_data = transmitters.data
+    try {
+        var [target_lat, target_lng] = [0,0]
+        if (req.query.ip) {
+            const lookup_result = geoip.lookup(req.query.ip)                
+            if (!lookup_result || lookup_result.country.toUpperCase() != "ID") {
+                return res.status(200).json({"region":"N/A"})
+            }
+            [target_lat, target_lng] = lookup_result.ll
+        } else if (req.query.lat && req.query.lng) {        
+            const lookup_result = await nominatim.reverse({lat: parseFloat(req.query.lat), lon: parseFloat(req.query.lng), zoom: 16})                
+            if (lookup_result.error || lookup_result.address.country_code.toUpperCase() != "ID") {
+                return res.status(200).json({"region":"N/A"})
+            }
+            [target_lat, target_lng] = [parseFloat(req.query.lat), parseFloat(req.query.lng)]
+        } else {
+            const lookup_result = geoip.lookup(req.ip)         
+            if (!lookup_result || lookup_result.country != "ID") {
+                return res.status(200).json({"region":"N/A"})            
+            }
+            [target_lat, target_lng] = lookup_result.ll
+        }        
+        const transmitters = await axios.get("https://digitaltv.kominfo.go.id/executive/get_antenna_map")
+        const tx_data = transmitters.data
 
-    var near_tx = {}
-    var min_distance = -1
+        var near_tx = {}
+        var min_distance = -1
 
-    for (i = 0; i<tx_data.data.length; i++) {
-        const tx_loc = [tx_data.data[i].lat, tx_data.data[i].lng]
-        const new_dist = getNearestDistance(target_lat, target_lng, tx_loc[0], tx_loc[1], "K")
+        for (i = 0; i<tx_data.data.length; i++) {
+            const tx_loc = [tx_data.data[i].lat, tx_data.data[i].lng]
+            const new_dist = getNearestDistance(target_lat, target_lng, tx_loc[0], tx_loc[1], "K")
 
-        if (min_distance == -1 || new_dist <= min_distance) {
-            min_distance = new_dist
-            near_tx = tx_data.data[i]
+            if (min_distance == -1 || new_dist <= min_distance) {
+                min_distance = new_dist
+                near_tx = tx_data.data[i]
+            }
         }
+
+        const coverage = await axios.post("https://digitaltv.kominfo.go.id/executive/get_coverage_map", `antenna[]=${near_tx.id}`)
+        return res.status(200).json({"region":coverage.data.data[0].area_service_name})
+    } catch (e) {
+        return res.status(500).json(e)
     }
-
-    const coverage = await axios.post("https://digitaltv.kominfo.go.id/executive/get_coverage_map", `antenna[]=${near_tx.id}`)
-    return res.status(200).json({"region":coverage.data.data[0].area_service_name})
 })
 
 app.get("/dtv/id/area_channels", async (req, res) => {
-    var [target_lat, target_lng] = [0,0]
-    if (req.query.ip) {
-        const lookup_result = geoip.lookup(req.query.ip)                
-        if (!lookup_result || lookup_result.country.toUpperCase() != "ID") {
-            return res.status(200).json({"transmitters":[],"channels":[]})
-        }
-        [target_lat, target_lng] = lookup_result.ll
-    } else if (req.query.lat && req.query.lng) {
-        const lookup_result = await nominatim.reverse({lat: parseFloat(req.query.lat), lon: parseFloat(req.query.lng)})
-        if (lookup_result.error || lookup_result.address.country_code.toUpperCase() != "ID") {
-            return res.status(200).json({"transmitters":[],"channels":[]})
-        }
-        [target_lat, target_lng] = [parseFloat(req.query.lat), parseFloat(req.query.lng)]
-    } else {
-        const lookup_result = geoip.lookup(req.ip)         
-        if (!lookup_result || lookup_result.country != "ID") {
-            return res.status(200).json({"transmitters":[],"channels":[]})          
-        }
-        [target_lat, target_lng] = lookup_result.ll
-    }        
-    const transmitters = await axios.get("https://digitaltv.kominfo.go.id/executive/get_antenna_map")
-    const tx_data = transmitters.data
+    try {
+        var [target_lat, target_lng] = [0,0]
+        if (req.query.ip) {
+            const lookup_result = geoip.lookup(req.query.ip)                
+            if (!lookup_result || lookup_result.country.toUpperCase() != "ID") {
+                return res.status(200).json({"transmitters":[],"channels":[]})
+            }
+            [target_lat, target_lng] = lookup_result.ll
+        } else if (req.query.lat && req.query.lng) {
+            const lookup_result = await nominatim.reverse({lat: parseFloat(req.query.lat), lon: parseFloat(req.query.lng)})
+            if (lookup_result.error || lookup_result.address.country_code.toUpperCase() != "ID") {
+                return res.status(200).json({"transmitters":[],"channels":[]})
+            }
+            [target_lat, target_lng] = [parseFloat(req.query.lat), parseFloat(req.query.lng)]
+        } else {
+            const lookup_result = geoip.lookup(req.ip)         
+            if (!lookup_result || lookup_result.country != "ID") {
+                return res.status(200).json({"transmitters":[],"channels":[]})          
+            }
+            [target_lat, target_lng] = lookup_result.ll
+        }        
+        const transmitters = await axios.get("https://digitaltv.kominfo.go.id/executive/get_antenna_map")
+        const tx_data = transmitters.data
 
-    var near_tx = {}
-    var min_distance = -1
+        var near_tx = {}
+        var min_distance = -1
 
-    for (i = 0; i<tx_data.data.length; i++) {
-        const tx_loc = [tx_data.data[i].lat, tx_data.data[i].lng]
-        const new_dist = getNearestDistance(target_lat, target_lng, tx_loc[0], tx_loc[1], "K")
+        for (i = 0; i<tx_data.data.length; i++) {
+            const tx_loc = [tx_data.data[i].lat, tx_data.data[i].lng]
+            const new_dist = getNearestDistance(target_lat, target_lng, tx_loc[0], tx_loc[1], "K")
 
-        if (min_distance == -1 || new_dist <= min_distance) {
-            min_distance = new_dist
-            near_tx = tx_data.data[i]
+            if (min_distance == -1 || new_dist <= min_distance) {
+                min_distance = new_dist
+                near_tx = tx_data.data[i]
+            }
         }
+
+        const coverage = await axios.post("https://digitaltv.kominfo.go.id/executive/get_coverage_map", `antenna[]=${near_tx.id}`)
+        return res.status(200).json({"transmitters":coverage.data.transmitter_in_area,"channels":coverage.data.channel_in_area})
+    } catch (e) {
+        return res.status(500).json(e)
     }
-
-    const coverage = await axios.post("https://digitaltv.kominfo.go.id/executive/get_coverage_map", `antenna[]=${near_tx.id}`)
-    return res.status(200).json({"transmitters":coverage.data.transmitter_in_area,"channels":coverage.data.channel_in_area})
 })
 
 app.get("/dtv/id/channels", async (req, res) => {
-    var [target_lat, target_lng] = [0,0]
-    if (req.query.ip) {
-        const lookup_result = geoip.lookup(req.query.ip)                
-        if (!lookup_result || lookup_result.country.toUpperCase() != "ID") {
-            return res.status(200).json({"mux_id": "out-of-country", "alias": [], "transmitters": [],"channels": []})
-        }
-        [target_lat, target_lng] = lookup_result.ll
-    } else if (req.query.lat && req.query.lng) {
-        const lookup_result = await nominatim.reverse({lat: parseFloat(req.query.lat), lon: parseFloat(req.query.lng)})
-        if (lookup_result.error || lookup_result.address.country_code.toUpperCase() != "ID") {
-            return res.status(200).json({"mux_id": "out-of-country", "alias": [], "transmitters": [],"channels": []})
-        }
-        [target_lat, target_lng] = [parseFloat(req.query.lat), parseFloat(req.query.lng)]
-    } else {
-        const lookup_result = geoip.lookup(req.ip) 
-        if (!lookup_result || lookup_result.country != "ID") {
-            return res.status(200).json({"mux_id": "out-of-country", "alias": [], "transmitters": [],"channels": []})
-        }
-        [target_lat, target_lng] = lookup_result.ll
-    }        
-    const transmitters = await axios.get("https://digitaltv.kominfo.go.id/executive/get_antenna_map")
-    const tx_data = transmitters.data
+    try {
+        var [target_lat, target_lng] = [0,0]
+        if (req.query.ip) {
+            const lookup_result = geoip.lookup(req.query.ip)                
+            if (!lookup_result || lookup_result.country.toUpperCase() != "ID") {
+                return res.status(200).json({"mux_id": "out-of-country", "alias": [], "transmitters": [],"channels": []})
+            }
+            [target_lat, target_lng] = lookup_result.ll
+        } else if (req.query.lat && req.query.lng) {
+            const lookup_result = await nominatim.reverse({lat: parseFloat(req.query.lat), lon: parseFloat(req.query.lng)})
+            if (lookup_result.error || lookup_result.address.country_code.toUpperCase() != "ID") {
+                return res.status(200).json({"mux_id": "out-of-country", "alias": [], "transmitters": [],"channels": []})
+            }
+            [target_lat, target_lng] = [parseFloat(req.query.lat), parseFloat(req.query.lng)]
+        } else {
+            const lookup_result = geoip.lookup(req.ip) 
+            if (!lookup_result || lookup_result.country != "ID") {
+                return res.status(200).json({"mux_id": "out-of-country", "alias": [], "transmitters": [],"channels": []})
+            }
+            [target_lat, target_lng] = lookup_result.ll
+        }        
+        const transmitters = await axios.get("https://digitaltv.kominfo.go.id/executive/get_antenna_map")
+        const tx_data = transmitters.data
 
-    var near_tx = {}
-    var min_distance = -1
+        var near_tx = {}
+        var min_distance = -1
 
-    for (i = 0; i<tx_data.data.length; i++) {
-        const tx_loc = [tx_data.data[i].lat, tx_data.data[i].lng]
-        const new_dist = getNearestDistance(target_lat, target_lng, tx_loc[0], tx_loc[1], "K")
+        for (i = 0; i<tx_data.data.length; i++) {
+            const tx_loc = [tx_data.data[i].lat, tx_data.data[i].lng]
+            const new_dist = getNearestDistance(target_lat, target_lng, tx_loc[0], tx_loc[1], "K")
 
-        if (min_distance == -1 || new_dist <= min_distance) {
-            min_distance = new_dist
-            near_tx = tx_data.data[i]
-        }
-    }
-
-    const coverage = await axios.post("https://digitaltv.kominfo.go.id/executive/get_coverage_map", `antenna[]=${near_tx.id}`)
-    const service_id = coverage.data.data[0].area_service_name.replace(/[^ -~]+/g, "").replace(/ /g, "-").toLowerCase()
-    
-    var area = areas[service_id]
-    if (!area) {
-        const tv_tx = []
-        const v_tx = coverage.data.transmitter_in_area
-        const tv_tx_ids = {}
-
-        for (i = 0; i<v_tx.length; i++) {
-            tv_tx.push({"id": parseInt(v_tx[i].id), "mux_id": "", "alias": [], "website": "", "name": v_tx[i].name, "frequency": v_tx[i].frequency, "uhf_channel": v_tx[i].channel, "lat": v_tx[i].latitude, "lng": v_tx[i].longitude})
-            tv_tx_ids[v_tx[i].name] = v_tx[i].id
+            if (min_distance == -1 || new_dist <= min_distance) {
+                min_distance = new_dist
+                near_tx = tx_data.data[i]
+            }
         }
 
-        const tv_ch = []
-        const v_ch = coverage.data.channel_in_area
-        for (i = 0; i<v_ch.length; i++) {
-            tv_ch.push({"channel_name": v_ch[i].name, "channel_id": "", "epg_id": "", "alias": [], "stream_urls": [], "parent": null, "via": parseInt(tv_tx_ids[v_ch[i].antenna]), "quality": v_ch[i].quality})
-        }
-
-        area = {"mux_id": service_id, "alias": [], "transmitters":tv_tx,"channels":tv_ch}
-        areas[service_id] = area
+        const coverage = await axios.post("https://digitaltv.kominfo.go.id/executive/get_coverage_map", `antenna[]=${near_tx.id}`)
+        const service_id = coverage.data.data[0].area_service_name.replace(/[^ -~]+/g, "").replace(/ /g, "-").toLowerCase()
         
-        await fs.writeFile("./areas.json", JSON.stringify(areas, null, 4))
-        return res.status(200).json(area)
-    } else {
-        return res.status(200).json(area)
+        var area = areas[service_id]
+        if (!area) {
+            const tv_tx = []
+            const v_tx = coverage.data.transmitter_in_area
+            const tv_tx_ids = {}
+
+            for (i = 0; i<v_tx.length; i++) {
+                tv_tx.push({"id": parseInt(v_tx[i].id), "mux_id": "", "alias": [], "website": "", "name": v_tx[i].name, "frequency": v_tx[i].frequency, "uhf_channel": v_tx[i].channel, "lat": v_tx[i].latitude, "lng": v_tx[i].longitude})
+                tv_tx_ids[v_tx[i].name] = v_tx[i].id
+            }
+
+            const tv_ch = []
+            const v_ch = coverage.data.channel_in_area
+            for (i = 0; i<v_ch.length; i++) {
+                tv_ch.push({"channel_name": v_ch[i].name, "channel_id": "", "epg_id": "", "alias": [], "stream_urls": [], "parent": null, "via": parseInt(tv_tx_ids[v_ch[i].antenna]), "quality": v_ch[i].quality})
+            }
+
+            area = {"mux_id": service_id, "alias": [], "transmitters":tv_tx,"channels":tv_ch}
+            areas[service_id] = area
+            
+            await fs.writeFile("./areas.json", JSON.stringify(areas, null, 4))
+            return res.status(200).json(area)
+        } else {
+            return res.status(200).json(area)
+        }
+    } catch (e) {
+        return res.status(500).json(e)
     }
 })
 
